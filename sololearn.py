da5f2a50914f71b9da2894860895be02a0a7e312
Murat Nurkassymov <boing416@gmail.com>
2019-05-05 14:38:06 -0700

    Update sololearn.py

--- sololearn.py
+++ sololearn.py
@@ -1,1 +1,13 @@
+>>> print('Hello world!')
+Hello world!
+//Simple Operations
+>>> 2 + 2
+4
+>>> 5 + 4 - 3
+6
+//STRINGS
+>>> "Python is fun!"
+'Python is fun!'
+>>> 'Always look on the bright side of life'
+'Always look on the bright side of life'
 
